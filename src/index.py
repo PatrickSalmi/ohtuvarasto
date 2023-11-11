be9081445caa796ebee9245cc703344f8db4ef97
PatrickSalmi <patrick.salmi@helsinki.fi>
2023-11-11 17:25:54 +0200

    more GHA pylint testing

--- src/index.py
+++ src/index.py
@@ -66,7 +66,5 @@ def main():
     varastoon_lisays(mehua, olutta)
     varastosta_ottaminen(mehua, olutta)
 
-#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
 if __name__ == "__main__":
     main()
