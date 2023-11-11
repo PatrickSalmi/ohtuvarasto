83144a0045cb3be32f1aed44283487b343aeaac8
PatrickSalmi <patrick.salmi@helsinki.fi>
2023-11-11 17:13:53 +0200

    added week 2 exercise 3

--- src/index.py
+++ src/index.py
@@ -1,19 +1,17 @@
 from varasto import Varasto
 
-
-def main():
-    mehua = Varasto(100.0)
-    olutta = Varasto(100.0, 20.2)
-
+def alku_tiedot(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
 
+def olut_getterit(olutta):
     print("Olut getterit:")
     print(f"saldo = {olutta.saldo}")
     print(f"tilavuus = {olutta.tilavuus}")
     print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
 
+def mehu_setterit(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,7 +20,7 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
-    print("Virhetilanteita:")
+def luokka_attribuutit():
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
     print(huono)
@@ -31,6 +29,7 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def varastoon_lisays(mehua, olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
@@ -41,6 +40,7 @@ def main():
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+def varastosta_ottaminen(mehua, olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
@@ -52,6 +52,19 @@ def main():
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
+
+def main():
+    mehua = Varasto(100.0)
+    olutta = Varasto(100.0, 20.2)
+
+    alku_tiedot(mehua, olutta)
+    olut_getterit(olutta)
+    mehu_setterit(mehua)
+
+    print("Virhetilanteita:")
+    luokka_attribuutit()
+    varastoon_lisays(mehua, olutta)
+    varastosta_ottaminen(mehua, olutta)
 
 
 if __name__ == "__main__":
